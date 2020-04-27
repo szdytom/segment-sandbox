307397f06e9170fb46fc4850f8c17fb173432165
szdytom <szdytom@163.com>
2020-04-27 12:55:17 +0800

    rm useless item

--- sandbox.h
+++ sandbox.h
@@ -22,7 +22,6 @@ typedef int (*container_func)(void *arg);
 typedef struct {
 	char *work_dir;    /* mount namespace to here */
 	char *rootfs_dir;  /* mount namespace from here */
-	char *hostname;    /* UTC namespace hostname */
 
 	int stack_size;    /* stack size of new program, in bytes */
 	container_func function; /* function to run inside sandbox */
