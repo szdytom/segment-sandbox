90b710a928958e981a8ec1baefbbf3eb2b96000b
szdytom <szdytom@163.com>
2020-07-20 15:58:07 +0800

    add comment

--- include/sandbox.h
+++ include/sandbox.h
@@ -33,7 +33,7 @@ struct sandbox_t {
     int stack_size;          /* stack size of new program, in bytes */
     container_func function; /* function to run inside sandbox */
     void *func_args;         /* fnuction's args */
-	std::string hostname;
+	std::string hostname;    /* hostname inside container */
 };
 
 void create_sandbox(std::shared_ptr<sandbox_t> cfg);
