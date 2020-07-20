871210cc013d7ffec9140a3666dbd5f1d4c642a1
szdytom <szdytom@163.com>
2020-07-20 15:04:36 +0800

    Add entry_handle for change hostname

--- src/sandbox.cc
+++ src/sandbox.cc
@@ -9,11 +9,15 @@ Define of functions in sandbox.h
  * @param cfg sandbox config
  * @return int the cfg->function return
  * 
- * Not public.
+ * Not publicfor user to use.
  * Config settings inside container.
  * 1. Set Host Name
- * 2. 
  */
+int entry_handle(void *cfg_ptr) {
+	std::shared_ptr<ssandbox::sandbox_t> cfg = *((std::shared_ptr<ssandbox::sandbox_t>*)cfg_ptr);
+	sethostname(cfg->hostname.c_str(), cfg->hostname.size());
+	return cfg->function(cfg->func_args);
+}
 
 /**
  * @brief Create A New Sandbox
@@ -22,14 +26,16 @@ Define of functions in sandbox.h
  * 
  * create a sandbox to run cfg->function.
 */
-void create_sandbox(std::shared_ptr<ssandbox::sandbox_t> cfg) {
+void ssandbox::create_sandbox(std::shared_ptr<ssandbox::sandbox_t> cfg) {
     /* create stack space for child to use */
     /* new will throw out a alloc_error if fail, so we don't need to handle nullptr */
     std::unique_ptr<char[]> container_stack(new char[cfg->stack_size]);
     char *container_stack_ptr = container_stack.get();
 
-    int container_pid = clone(cfg->function, container_stack_ptr + cfg->stack_size /* reverse memory */,
-                              SIGCHLD | CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID, cfg->func_args);
+    int container_pid = clone((ssandbox::container_func)entry_handle, 
+							  container_stack_ptr + cfg->stack_size, /* reverse memory */
+                              SIGCHLD | CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID, 
+							  (void*)&(cfg));
 
     if (container_pid == -1)
         /* clone process failed */
--- test/runbash.cc
+++ test/runbash.cc
@@ -9,7 +9,6 @@ char* container_args[] = {
 
 int func(void *args) {
 	printf("Inside countainer [%05d]!\n", getpid());
-	sethostname("container", 10);
 
 	char **xargs = (char**)args;
 	printf("+%s\n", xargs[0]);
@@ -17,14 +16,15 @@ int func(void *args) {
 	printf("exiting\n");
 }
 
-int main(void) {
-	sandbox_t cfg;
-	cfg.function = func;
-	cfg.func_args = container_args;
-	cfg.stack_size = 1024 * 1024; // 1MB
+int main() {
+	shared_ptr<sandbox_t> cfg(new sandbox_t);
+	cfg->function = func;
+	cfg->func_args = container_args;
+	cfg->stack_size = 1024 * 1024; // 1MB
+	cfg->hostname = "container";
 	
 	printf("Outside\n");
-	create_sandbox(shared_ptr<sandbox_t>(&cfg));
+	create_sandbox(cfg);
 	printf("Returned to father!\n");
 	return 0;
 }
