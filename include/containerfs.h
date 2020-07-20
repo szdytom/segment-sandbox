15aa186a77168e4117dd5bf22da818f2d1ac53ff
szdytom <szdytom@163.com>
2020-07-20 16:34:00 +0800

    add component containerfs
    Signed-off-by: szdytom <szdytom@163.com>

---
+++ include/containerfs.h
@@ -0,0 +1,23 @@
+/**
+ * containerfs.h
+ *
+ * This file is about mount filesystem inside container
+ *
+ * 
+ */
+
+#ifndef CONTAINERFS_H
+#define CONTAINERFS_H
+
+#include <filesystem>
+
+namespace ssandbox {
+
+struct fsconfig_t {
+	std::filesystem::path root;
+	std::filesystem::path work;
+};
+
+} /* namespace ssandbox */
+
+#endif /* CONTAINERFS_H */
