83f25f77a53744e0a00eaf2457ee2d7712e498a0
szdytom <szdytom@163.com>
2020-07-27 11:28:05 +0800

    change to a better config

--- include/containerfs.h
+++ include/containerfs.h
@@ -13,12 +13,17 @@
 
 namespace ssandbox {
 
-class FileSystemConfig {
-    public:
-    FileSystemConfig() {}
-    virtual ~FileSystemConfig() = 0;
-    virtual void mount() = 0;
-    virtual void chroot() = 0;
+class MountInfo {
+    /* will be mount to here in you host machine */
+    std::filesystem::path work;
+    /* Mount source in your host machine */
+    std::filesystem::path root_mount_point;
+    
+    /* An extra point to mount, will be mounted into /mnt inside container*/
+    std::filesystem::path extra_point;
+    
+    bool mount_proc;
+    bool mount_tmp;
 };
 
 } /* namespace ssandbox */
