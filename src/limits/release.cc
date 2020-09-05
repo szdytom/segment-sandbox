9097871d7fc1b331f3ba3e6356cb1a70af52975c
szdytom <szdytom@163.com>
2020-09-05 10:43:04 +0800

    add release

---
+++ src/limits/release.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <fmt/format.h>
+#include "ssandbox/cgroup.h"
+#include "ssandbox/limits.h"
+
+void ssandbox::LimitsMgr::release() {
+    for (ssandbox::CgroupUnit* c : this->_used_cgroup) {
+        c->remove();
+        delete c;
+    }
+
+    this->_used_cgroup.clear();
+}
