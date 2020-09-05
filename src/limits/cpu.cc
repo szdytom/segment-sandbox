4e8f0b12801745a080c6bbb3f073136bf8972323
szdytom <szdytom@163.com>
2020-09-05 10:42:22 +0800

    add cpu limit

---
+++ src/limits/cpu.cc
@@ -0,0 +1,15 @@
+#include <string>
+#include <fmt/format.h>
+#include "ssandbox/cgroup.h"
+#include "ssandbox/limits.h"
+
+void ssandbox::LimitsMgr::cpu(unsigned int limit) {
+    auto cs = ssandbox::CgroupSubsystem("cpu");
+    auto c = cs.create(fmt::format("ssandbox_{}", this->_uid));
+
+    c->write("cpu.cfs_period_us", "1000000");
+    c->write("cpu.cfs_quota_us", std::to_string(limit * 10000));
+    c->write("tasks", std::to_string(this->_pid));
+
+    this->_used_cgroup.push_back(c);
+}
