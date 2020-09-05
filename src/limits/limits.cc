7e3d1d7c97072506af1c967997cef8cf4da1ff50
szdytom <szdytom@163.com>
2020-09-05 10:43:12 +0800

    add other functions

---
+++ src/limits/limits.cc
@@ -0,0 +1,21 @@
+#include "ssandbox/limits.h"
+#include <exception>
+#include <stdexcept>
+#include <string>
+#include <fmt/format.h>
+#include "ssandbox/utils/process.h"
+
+ssandbox::LimitsMgr::LimitsMgr(std::string uid) {
+    this->_uid = uid;
+}
+
+ssandbox::LimitsMgr::~LimitsMgr() {
+    this->release();
+}
+
+void ssandbox::LimitsMgr::task(pid_t pid) {
+    if (!ssandbox::utils::process::checkProcessAlive(pid))
+        throw std::logic_error(fmt::format("[Segment Sandbox - {}] Process ({}) not found", __FUNCTION__, pid));
+
+    this->_pid = pid;
+}
