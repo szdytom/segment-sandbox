0ff717e0389c344ec17143235e76f74e40a2d3b0
szdytom <szdytom@163.com>
2020-09-05 12:15:18 +0800

    fix

--- src/limits/time.cc
+++ src/limits/time.cc
@@ -1,34 +1,34 @@
+#include <chrono>
 #include <future>
 #include <string>
 #include <thread>
-#include <chrono>
 #include <fmt/format.h>
 #include <signal.h>
 #include "ssandbox/cgroup.h"
 #include "ssandbox/limits.h"
 #include "ssandbox/utils/process.h"
 
-bool ssandbox::LimitsMgr::_watchTimeLimit() {
+void ssandbox::LimitsMgr::_watchTimeLimit() {
     const unsigned int wait_time_ms = 50U;
     const unsigned int extra_time = 500U;
-    const auto limit = std::chrono::microseconds(this->_time_limit + extra_time);
+    const auto limit = std::chrono::milliseconds(this->_time_limit + extra_time);
     auto start_time_clock = std::chrono::steady_clock::now();
 
     while (true) {
         std::this_thread::sleep_for(std::chrono::microseconds(wait_time_ms));
         if (!ssandbox::utils::process::checkProcessAlive(this->_pid))
-            return false;
+            return;
 
         auto delta = std::chrono::steady_clock::now() - start_time_clock;
-        if (delta > limit) {
+        if (limit < delta) {
             kill(this->_pid, SIGKILL);
-            return true;
+            return;
         }
     }
 }
 
-std::future<bool>& ssandbox::LimitsMgr::time(unsigned int limit) {
+void ssandbox::LimitsMgr::time(unsigned int limit) {
     this->_time_limit = limit;
-    static auto surveillant = std::async([this] { return this->_watchTimeLimit(); });
-    return surveillant;
+    this->_time_surveillant = std::async([this] { this->_watchTimeLimit(); });
+    this->_time_surveillant_mark = true;
 }
