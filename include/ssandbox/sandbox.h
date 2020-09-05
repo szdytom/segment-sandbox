3985ec63c04aa6e9179ea4fdf586dae6546aef99
szdytom <szdytom@163.com>
2020-09-05 12:14:30 +0800

    add limit type

--- include/ssandbox/sandbox.h
+++ include/ssandbox/sandbox.h
@@ -26,24 +26,26 @@
 
 namespace ssandbox {
 
-typedef int (*container_func)(void *arg);
+typedef int (*container_func)(void* arg);
 
-struct sandbox_t {
-    int stack_size;          /* stack size of new program, in bytes */
-    container_func function; /* function to run inside sandbox */
-    void *func_args;         /* fnuction's args */
-    std::string hostname;    /* hostname inside container */
-    MountInfo mnt_config;    /* Mount Info  */
+struct sandbox_limit_t {
+    unsigned int cpu;
+    unsigned int time;
+    unsigned long long memory;
 };
 
-struct sandbox_handle_t {
-    sandbox_t *sandbox;
-    int uid;
-    int gid;  
+struct sandbox_t {
+    std::string uid;              /* an unique ID for this sandbox */
+    int stack_size;               /* stack size of new program, in bytes */
+    container_func function;      /* function to run inside sandbox */
+    void* func_args;              /* fnuction's args */
+    std::string hostname;         /* hostname inside container */
+    MountInfo mnt_config;         /* Mount Config */
+    sandbox_limit_t limit_config; /* Resource Limit Config */
 };
 
 void create_sandbox(std::shared_ptr<sandbox_t> cfg);
-int entry_handle(void *cfg_ptr);
+int entry_handle(void* cfg_ptr);
 
 } // namespace ssandbox
 
