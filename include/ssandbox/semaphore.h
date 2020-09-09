1ce1f561dc8b7d736f3c1036ede7ed54a3af7099
szdytom <szdytom@163.com>
2020-09-09 18:52:20 +0800

    add semaphore

---
+++ include/ssandbox/semaphore.h
@@ -0,0 +1,27 @@
+// -*- C++ -*- Part of the Segment Sandbox Project
+
+#ifndef SSANDBOX_SEMAPHORE_H
+#define SSANDBOX_SEMAPHORE_H
+
+#include <future>
+#include <memory>
+
+namespace ssandbox {
+
+class semaphore {
+public:
+    void create();
+
+    void post(int val);
+    void wait();
+    bool wait_for(unsigned int time_limit);
+    int get();
+
+private:
+    std::shared_ptr<std::promise<int>> _target;
+    std::future<int> _future;
+};
+
+}; // namespace ssandbox
+
+#endif // SSANDBOX_SEMAPHORE_H
---
+++ src/semaphore.cc
@@ -0,0 +1,23 @@
+#include "ssandbox/semaphore.h"
+#include <chrono>
+
+void ssandbox::semaphore::create() {
+    this->_target.reset(new std::promise<int>());
+    this->_future = this->_target->get_future();
+}
+
+void ssandbox::semaphore::post(int val) {
+    this->_target->set_value(val);
+}
+
+void ssandbox::semaphore::wait() {
+    this->_future.wait();
+}
+
+bool ssandbox::semaphore::wait_for(unsigned int time_limit) {
+    return this->_future.wait_for(std::chrono::microseconds(time_limit)) == std::future_status::ready;
+}
+
+int ssandbox::semaphore::get() {
+    return this->_future.get();
+}
