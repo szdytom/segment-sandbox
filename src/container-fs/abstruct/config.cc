2aed9d4941bc93553c7de66a5b3998df9f0946ce
szdytom <szdytom@163.com>
2020-09-09 13:09:12 +0800

    add fs works

--- src/container-fs/abstruct/config.cc
+++ src/container-fs/abstruct/config.cc
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <fmt/core.h>
 #include "ssandbox/containerfs.h"
 
@@ -14,13 +15,23 @@ void ssandbox::AbstructContainerFS::setUID(const std::string& uid) {
 }
 
 void ssandbox::AbstructContainerFS::setWorkspace(const std::filesystem::path& workspace_path) {
+    if (!std::filesystem::exists(workspace_path))
+        std::filesystem::create_directories(workspace_path);
+
     this->workspace = workspace_path;
 }
 
 void ssandbox::AbstructContainerFS::setImage(const std::filesystem::path& image_path) {
+    if (!std::filesystem::exists(image_path))
+        throw std::runtime_error(fmt::format("[Segment Sandobx - {}] Image Directory '{}' not found.", __FUNCTION__, image_path.string()));
+
     this->image_path = image_path;
 }
 
 std::filesystem::path ssandbox::AbstructContainerFS::getFSPath(const std::filesystem::path& name) {
-    return this->workspace / name;
+    std::filesystem::path res = this->workspace / this->uid / name;
+    if (!std::filesystem::exists(res))
+        std::filesystem::create_directories(res);
+
+    return res;
 }
