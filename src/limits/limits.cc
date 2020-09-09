1255deb342ac148619c89e3bd144ee5024b2a6b9
szdytom <szdytom@163.com>
2020-09-09 13:34:18 +0800

    use getSSErrorMsg

--- src/limits/limits.cc
+++ src/limits/limits.cc
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <string>
 #include <fmt/core.h>
+#include "ssandbox/utils/exceptions.h"
 #include "ssandbox/utils/process.h"
 
 ssandbox::LimitsMgr::LimitsMgr(std::string uid) {
@@ -16,7 +17,7 @@ ssandbox::LimitsMgr::~LimitsMgr() {
 
 void ssandbox::LimitsMgr::task(pid_t pid) {
     if (!ssandbox::utils::process::checkProcessAlive(pid))
-        throw std::logic_error(fmt::format("[Segment Sandbox - {}] Process ({}) not found", __FUNCTION__, pid));
+        throw std::logic_error(ssandbox::utils::exceptions::getSSErrorMsg(fmt::format("Process {} not found or already dead.", pid), __FUNCTION__));
 
     this->_pid = pid;
 }
