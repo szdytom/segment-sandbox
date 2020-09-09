114dbc29141cfce1a8d61a4e9c65f66039748a67
szdytom <szdytom@163.com>
2020-09-09 13:11:01 +0800

    remove old ver

--- include/ssandbox/containerfs.h
+++ include/ssandbox/containerfs.h
@@ -9,26 +9,6 @@
 
 namespace ssandbox {
 
-struct MountInfo {
-    /* will be mount to here in you host machine */
-    std::filesystem::path point;
-    /* Mount source in your host machine (Lower Directory) */
-    std::filesystem::path lower_dir;
-    /* Temp Working Dir for overlay */
-    std::filesystem::path workspace;
-    /* Upper Directory */
-    std::filesystem::path upper_dir;
-
-    /* An extra point to mount, will be mounted into /mnt inside container*/
-    std::filesystem::path extra_point;
-
-    /* If enabled, it will mount /proc */
-    bool mount_proc;
-
-    /* If enabled, it will mount /tmp */
-    bool mount_tmp;
-};
-
 class AbstructContainerFS {
 public:
     void mountAll();
@@ -71,9 +51,6 @@ protected:
     void mountMain();
     void umountMain();
 };
-
-void mount_containerfs(MountInfo cfg);
-void umount_containerfs(MountInfo cfg);
 
 } /* namespace ssandbox */
 
