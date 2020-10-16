71e9af8b60066943393ab45a606c624cc73f5ee7
szdytom <szdytom@163.com>
2020-10-16 10:25:17 +0800

    Remove directory creations
    Signed-off-by: szdytom <szdytom@163.com>

--- src/container-fs/abstruct/procfs.cc
+++ src/container-fs/abstruct/procfs.cc
@@ -5,9 +5,6 @@
 #include "ssandbox/utils/exceptions.h"
 
 void ssandbox::AbstructContainerFS::_mount_proc() {
-    if (mkdir("/proc", S_IRWXU | S_IRWXG | S_IRWXO))
-        throw ssandbox::exceptions::syscall_error(errno, "Cannot make directory for /proc", __FUNCTION__);
-
     if (mount("proc", "/proc", "proc", 0, nullptr))
         throw ssandbox::exceptions::syscall_error(errno, "Cannot mount fs of proc", __FUNCTION__);
 }
@@ -16,7 +13,4 @@ void ssandbox::AbstructContainerFS::_umount_proc() {
     std::string proc_path((this->get_fs_path("target") / "proc").string());
     if (umount2(proc_path.c_str(), MNT_FORCE))
         throw ssandbox::exceptions::syscall_error(errno, "Cannot umount fs of proc", __FUNCTION__);
-
-    if (rmdir(proc_path.c_str()))
-        throw ssandbox::exceptions::syscall_error(errno, "Cannot remove directory /proc", __FUNCTION__);
 }
--- src/container-fs/abstruct/tmpfs.cc
+++ src/container-fs/abstruct/tmpfs.cc
@@ -5,9 +5,6 @@
 #include "ssandbox/utils/exceptions.h"
 
 void ssandbox::AbstructContainerFS::_mount_tmp() {
-    if (mkdir("/tmp", S_IRWXU | S_IRWXG | S_IRWXO))
-        throw ssandbox::exceptions::syscall_error(errno, "Cannot make directory for /tmp", __FUNCTION__);
-
     if (mount("tmpfs", "/tmp", "tmpfs", 0, nullptr))
         throw ssandbox::exceptions::syscall_error(errno, "Cannot mount fs of tmpfs", __FUNCTION__);
 }
@@ -16,7 +13,4 @@ void ssandbox::AbstructContainerFS::_umount_tmp() {
     std::string tmppath((this->get_fs_path("target") / "tmp").string());
     if (umount2(tmppath.c_str(), MNT_FORCE))
         throw ssandbox::exceptions::syscall_error(errno, "Cannot umount fs of tmpfs", __FUNCTION__);
-
-    if (rmdir(tmppath.c_str()))
-        throw ssandbox::exceptions::syscall_error(errno, "Cannot remove directory /tmp", __FUNCTION__);
 }
